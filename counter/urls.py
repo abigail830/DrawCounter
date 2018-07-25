b5c30b7cd25f3d1c7f893932af64374969fdc136
SaraQian <abigail830@163.com>
2018-07-26 00:04:41 +0800

    adding full member list and detail pages

--- counter/urls.py
+++ counter/urls.py
@@ -1,7 +1,10 @@
+from django.conf.urls import url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('health', views.health,  name='health'),
+    url(r'^detail/(?P<id>\d+)/$', views.detail, name='detail')
 ]
--- counter/views.py
+++ counter/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Member, Counter
+from django.template import loader
+
+
+def health(request):
+    return HttpResponse("I am Health!")
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    summary=[]
+    full_list = Member.objects.all()
+    for member_info in full_list:
+        member_detail = {
+            "id" : member_info.id,
+            "member_name":  member_info.member_name,
+            "remark": member_info.remark,
+            "count": Counter.objects.all().values().filter(member=member_info).latest('last_update_date')}
+        summary.append(member_detail)
+
+    template = loader.get_template('counter/members.html')
+    context = {
+        'summary': summary,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, id):
+
+    member = Member.objects.get(pk=id)
+
+    template = loader.get_template('counter/detail.html')
+    context = {
+            'name': member.member_name,
+            'join_date': member.join_date,
+            'remark': member.remark,
+            'history': Counter.objects.filter(member=member),
+    }
+
+    return HttpResponse(template.render(context, request))
